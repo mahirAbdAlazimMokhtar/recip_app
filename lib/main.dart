0fbf227c22affc75bcea539fa792509bbfd36bb3
mahirabdalazimmokhtar <mahr.bwb@gmial.com>
2021-06-29 18:15:48 +0200

    add themedata to main

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       home: DashboardScreen(),
     );
   }
