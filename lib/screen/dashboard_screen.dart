ff9723f13397a5fb0884a163951e58ee9517d05f
mahirabdalazimmokhtar <mahr.bwb@gmial.com>
2021-06-29 18:16:01 +0200

    add themedata to main

--- lib/screen/dashboard_screen.dart
+++ lib/screen/dashboard_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:recip_app/utils/responsive_layout.dart';
+import 'package:recip_app/widgets/wave_border_card.dart';
 
 class DashboardScreen extends StatelessWidget {
   Widget heightPlaceHolder(bool isMobileScreen) => SizedBox(
         height: isMobileScreen ? 20 : 30,
       );
+  List<Widget> widgetList = [
+    Text('first widget'),
+    Text('second widget'),
+  ];
   @override
   Widget build(BuildContext context) {
     const imageRecipeOfTheDay =
@@ -37,9 +42,10 @@ class DashboardScreen extends StatelessWidget {
           ),
           //this for list of widget
           SliverPadding(
-            padding: EdgeInsets.symmetric(
-              horizontal: isMobileScreen ? 20 : 40,
-              vertical: isMobileScreen ? 10 : 10,
+            padding: EdgeInsets.only(
+              top: isMobileScreen ? 40.0: 50.0,
+              bottom: isMobileScreen ? 40.0: 50.0,
+              left: isMobileScreen ? 20.0: 30.0,
             ),
             sliver: SliverList(
                 //this attr to give you list of widget
@@ -67,7 +73,9 @@ class DashboardScreen extends StatelessWidget {
                   constraints: BoxConstraints(maxHeight: 200,
                   ),
                   child: ListView.separated(
-                      itemBuilder: (context, index) => WaveBorderCard(),
+                      itemBuilder: (context, index) => WaveBorderCard(width: 200,
+                      recipeCardName: 'Real Data',
+                      ),
                       separatorBuilder: (context, index) => SizedBox(
                             width: 20,
                           ),
@@ -85,75 +93,3 @@ class DashboardScreen extends StatelessWidget {
   }
 }
 
-class WaveBorderCard extends StatelessWidget {
-  final String recipeCardName;
-  const WaveBorderCard({
-    Key? key,
-    this.imageRecipeOfTheDay = '',
-    this.isMobileScreen = true,
-    this.recipeCardName = "",
-  }) : super(key: key);
-
-  final String imageRecipeOfTheDay;
-  final bool isMobileScreen;
-
-  @override
-  Widget build(BuildContext context) {
-    var isMobileScreen = ResponsiveLayout.isSmallScreen(context);
-    const imageRecipeOfTheDay =
-        'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80';
-    return Stack(
-      children: [
-        Container(
-          height: 200,
-          width: 400,
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(
-                  30.0,
-                ),
-                topLeft: Radius.circular(
-                  30,
-                ),
-              ),
-              image: DecorationImage(
-                image: NetworkImage(imageRecipeOfTheDay),
-                fit: BoxFit.cover,
-              ),
-              boxShadow: [
-                const BoxShadow(
-                  color: Color(0x20000000),
-                  offset: Offset(3, 0),
-                  blurRadius: 0.4,
-                )
-              ]),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 0 , top: 150),
-          child: Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(
-              left: isMobileScreen ? 10 : 20,
-            ),
-            child: Text(
-              recipeCardName,
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            height: isMobileScreen ? 40 : 200,
-            width: isMobileScreen ? 300 : 500,
-            decoration: BoxDecoration(
-              color: const Color(
-                0xCCEEE6E6,
-              ),
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(30),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
