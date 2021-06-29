7f5b38b990f9449636a6ee5c0c87dc3979babdd8
mahirabdalazimmokhtar <mahr.bwb@gmial.com>
2021-06-29 06:53:57 +0200

    add const function to screen size

---
+++ lib/conast/constant.dart
@@ -0,0 +1,2 @@
+const imageRecipeOfTheDay =
+    'https://images.unsplash.com/photo-1612392062126-5cc76074df9c?ixid=MnwxMjA3fDF8MHxzZWFyY2h8NDh8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60';
--- lib/screen/dashboard_screen.dart
+++ lib/screen/dashboard_screen.dart
@@ -2,13 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:recip_app/utils/responsive_layout.dart';
 
 class DashboardScreen extends StatelessWidget {
-  const DashboardScreen({Key? key}) : super(key: key);
-
+  Widget hieghtPlaceHolder(bool isMobileScreen) => SizedBox(
+        height: isMobileScreen ? 20 : 40,
+      );
   @override
   Widget build(BuildContext context) {
+    const imageRecipeOfTheDay =
+        'https://images.unsplash.com/photo-1612392062126-5cc76074df9c?ixid=MnwxMjA3fDF8MHxzZWFyY2h8NDh8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60';
+    //for dont repet the code twice
+    var isMobileScreen = ResponsiveLayout.isSmallScreen(context);
     return Scaffold(
       body: CustomScrollView(
         //this is appBar for my app
+
         slivers: [
           SliverAppBar(
             stretch: true,
@@ -32,29 +38,102 @@ class DashboardScreen extends StatelessWidget {
           //this for list of widget
           SliverPadding(
             padding: EdgeInsets.symmetric(
-              horizontal: ResponsiveLayout.isSmallScreen(context) ? 20 : 40,
+              horizontal: isMobileScreen ? 20 : 40,
+              vertical: isMobileScreen ? 40 : 50,
             ),
             sliver: SliverList(
                 //this attr to give you list of widget
                 delegate: SliverChildListDelegate(
               [
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 25,
-                    top: 20,
-                  ),
-                  child: Text(
-                    'The recipe of the Day',
-                    style: TextStyle(
-                      fontSize: 20,
-                    ),
+                Text(
+                  'The recipe of the Day',
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
                 ),
+                hieghtPlaceHolder(isMobileScreen),
+                WaveBorderCard(
+                  imageRecipeOfTheDay: imageRecipeOfTheDay,
+                  isMobileScreen: isMobileScreen,
+                  recipeCardName: 'Pancakes',
+                )
               ],
             )),
           )
         ],
       ),
+    );
+  }
+}
+
+class WaveBorderCard extends StatelessWidget {
+  final String recipeCardName;
+  const WaveBorderCard({
+    Key? key,
+    required this.imageRecipeOfTheDay,
+    required this.isMobileScreen,
+    this.recipeCardName = "",
+  }) : super(key: key);
+
+  final String imageRecipeOfTheDay;
+  final bool isMobileScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    var isMobileScreen = ResponsiveLayout.isSmallScreen(context);
+    const imageRecipeOfTheDay =
+        'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80';
+    return Stack(
+      children: [
+        Container(
+          height: 200,
+          width: 400,
+          decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomRight: Radius.circular(
+                    30.0,
+                  ),
+                  topLeft: Radius.circular(
+                    30,
+                  )),
+              image: DecorationImage(
+                image: NetworkImage(imageRecipeOfTheDay),
+                fit: BoxFit.cover,
+              ),
+              boxShadow: [
+                const BoxShadow(
+                  color: Color(0x20000000),
+                  offset: Offset(3, 0),
+                  blurRadius: 0.4,
+                )
+              ]),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: isMobileScreen ? 150 : 32,
+          ),
+          child: Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(
+              left: isMobileScreen ? 10 : 20,
+            ),
+            child: Text(
+              recipeCardName,
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            height: isMobileScreen ? 40 : 200,
+            width: isMobileScreen ? 300 : 500,
+            decoration: BoxDecoration(
+              color: const Color(0xCCF7EBEB),
+              borderRadius: BorderRadius.only(
+                bottomRight: Radius.circular(30),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
